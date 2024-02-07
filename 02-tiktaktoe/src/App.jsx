d31f0ba966fe1f2342a0f8d0b8e983f00045c094
fedechaves <federicochavesf@gmail.com>
2024-02-07 10:00:00 +0100

    setting board

--- 02-tiktaktoe/src/App.jsx
+++ 02-tiktaktoe/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  const [board, setBoard] = useState(new Array(9).fill(null))
+
+  console.log(board)
 
   return (
-    <>
-      
-    </>
+    <section>
+      <h1>Tik Tak Toe</h1>
+        <div className='board'>
+
+        {board.map((_, index) => {
+          return <div  key={index}>{index}</div>
+        })}
+        </div>
+    </section>
   )
 }
 
