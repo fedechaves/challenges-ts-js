4f6d569b1d7e27eb712ecac4a7558b826f7e02c2
fedechaves <federicochavesf@gmail.com>
2024-02-08 10:00:00 +0100

    setting turns

--- 02-tiktaktoe/src/App.jsx
+++ 02-tiktaktoe/src/App.jsx
@@ -1,20 +1,47 @@
 import { useState } from 'react'
 import './App.css'
 
+const TURNS = {
+  x:'X',
+  o: 'O'
+}
+
 function App() {
   const [board, setBoard] = useState(new Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.x)
 
-  console.log(board)
+  function updateBoard ({ index }) {
+    const newTurn = turn === TURNS.x ? TURNS.x : TURNS.o
+    const newBoard = [...board]
 
+    newBoard[index] = newTurn
+  }
   return (
     <section>
       <h1>Tik Tak Toe</h1>
-        <div className='board'>
+        <div className='board' style={{ gap: "10px"}}>
 
-        {board.map((_, index) => {
-          return <div  key={index}>{index}</div>
+        {board.map((square, index) => {
+          return <div 
+                    onClick={() => {
+                      updateBoard({ index })
+                    }}  
+                    key={index}
+                    className='square'
+                    style={{ border: "1px solid red", height: "100px", width: "100px"}}
+                    >
+                      {board[index]}
+                 </div>
         })}
         </div>
+
+    <div>
+      <h1>TURNS</h1>
+      <div style={{ display:"flex", gap:"10px"}}>
+        <div className={turn === TURNS.x ? "is-selected" : ""}>{TURNS.x}</div>
+        <div className={turn === TURNS.o ? "is-selected" : ""}>{TURNS.o}</div>
+      </div>
+    </div>
     </section>
   )
 }
